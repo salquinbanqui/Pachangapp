c0ebdf783accb0f3d2f339eaae691e14a1bfda72
AitorArtero <93314392+AitorArtero@users.noreply.github.com>
2022-12-07 12:51:44 +0100

    avance grande en el layout de Tienda y Equipo en TriviaVentana

--- pachangappv2/src/GUI/PanelConFondo.java
+++ pachangappv2/src/GUI/PanelConFondo.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
@@ -7,24 +8,20 @@ import java.awt.Image;
 
 import javax.swing.JPanel;
 
-public class PanelConFondo extends JPanel {
+public class PanelConFondo extends JPanel{
 	Image imagen;
-	/**
-	 * Create the panel.
-	 */
+	
 	public PanelConFondo(Image i) {
 		imagen = i;
-		setLayout(new GridLayout(3, 3));
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
 		Graphics2D gr = (Graphics2D)g;
 		if(imagen == null) {
 			super.paintComponent(g);
 		}else {
-			gr.drawImage(imagen, 0, 0, this.getWidth(), this.HEIGHT, this);
+			gr.drawImage(imagen, 0, 0, this.getWidth(), this.getHeight(),this);
 		}
 	}
-
 }
